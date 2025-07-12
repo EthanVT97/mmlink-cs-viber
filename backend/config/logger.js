d85b98e3ec5c1ae5e3c5df2f01bda298300e653c
Ethan Victor <188495018+EthanVT97@users.noreply.github.com>
2025-07-12 12:55:06 +0630

    Create logger.js

---
+++ backend/config/logger.js
@@ -0,0 +1,14 @@
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/viber-bot.log' })
+  ]
+});
+
+export function createLogger(serviceName) {
+  return logger.child({ service: serviceName });
+}
